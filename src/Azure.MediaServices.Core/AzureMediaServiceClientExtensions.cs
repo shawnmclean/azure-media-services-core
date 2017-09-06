a786b2e8ed4ca18001af41d818eba4454547e309
Shawn Mclean <shawnrmclean@gmail.com>
2017-09-06 01:28:58 -0500

    Attempting to add blob work for creating asset from blob.

---
+++ src/Azure.MediaServices.Core/AzureMediaServiceClientExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.MediaServices.Core.Assets;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.RetryPolicies;
+
+namespace Azure.MediaServices.Core
+{
+  public static class AzureMediaServiceClientExtensions
+  {
+
+    public static async Task<Asset> CreateFromBlobAsync(this AzureMediaServiceClient client, CloudBlockBlob sourceBlob, StorageCredentials storageCredentials, CancellationToken cancellationToken) {
+      if (sourceBlob == null) {
+        throw new ArgumentNullException("sourceBlob", "The source blob cannot be null.");
+      }
+
+      if (storageCredentials == null) {
+        throw new ArgumentNullException("storageCredentials", "The destination storage credentials cannot be null.");
+      }
+
+      if (storageCredentials.IsAnonymous || storageCredentials.IsSAS) {
+        throw new ArgumentException("The destination storage credentials must contain the account key credentials.", "destinationStorageCredentials");
+      }
+
+      var asset = await client.CreateAsync(sourceBlob.Name, storageCredentials.AccountName, cancellationToken).ConfigureAwait(false);
+      cancellationToken.ThrowIfCancellationRequested();
+
+      IRetryPolicy retryPolicy = context.MediaServicesClassFactory.GetBlobStorageClientRetryPolicy().AsAzureStorageClientRetryPolicy();
+      BlobRequestOptions blobOptions = new BlobRequestOptions { RetryPolicy = retryPolicy };
+      CloudBlobContainer container = new CloudBlobContainer(asset.Uri, storageCredentials);
+      CloudBlockBlob blob = container.GetBlockBlobReference(sourceBlob.Name);
+
+      await CopyBlobHelpers.CopyBlobAsync(sourceBlob, blob, blobOptions, cancellationToken).ConfigureAwait(false);
+      cancellationToken.ThrowIfCancellationRequested();
+
+      var assetFile = await client.CreateAssetFile(sourceBlob.Name, cancellationToken).ConfigureAwait(false);
+
+      assetFile.IsPrimary = true;
+      if (sourceBlob.Properties != null) {
+        assetFile.ContentFileSize = sourceBlob.Properties.Length;
+        assetFile.MimeType = sourceBlob.Properties.ContentType;
+      }
+
+      await assetFile.UpdateAsync().ConfigureAwait(false);
+
+      return asset;
+    }
+  }
+}
---
+++ src/Azure.MediaServices.Core/CopyBlobHelpers.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Azure.MediaServices.Core
+{
+  /// <summary>
+  /// Contains helper methods for copying blobs.
+  /// </summary>
+  public static class CopyBlobHelpers
+  {
+    /// <summary>
+    /// Represents the maximum number of concurrent Copy Blob operations when copying content from a source container to a destination container.
+    /// </summary>
+    public const int MaxNumberOfConcurrentCopyFromBlobOperations = 750;
+
+    /// <summary>
+    /// Returns a <see cref="System.Threading.Tasks.Task"/> instance for the copy blobs operation from <paramref name="sourceContainer"/> to <paramref name="destinationContainer"/>.
+    /// </summary>
+    /// <param name="sourceContainer">The <see cref="Microsoft.WindowsAzure.Storage.Blob.CloudBlobContainer"/> instance that contains the blobs to be copied into <paramref name="destinationContainer"/>.</param>
+    /// <param name="destinationContainer">The <see cref="Microsoft.WindowsAzure.Storage.Blob.CloudBlobContainer"/> instance where the blobs from <paramref name="sourceContainer"/> will be copied.</param>
+    /// <param name="options">The <see cref="Microsoft.WindowsAzure.Storage.Blob.BlobRequestOptions"/>.</param>
+    /// <param name="cancellationToken">The <see cref="System.Threading.CancellationToken"/> instance used for cancellation.</param>
+    /// <returns>A <see cref="System.Threading.Tasks.Task"/> instance for the copy blobs operation from <paramref name="sourceContainer"/> to <paramref name="destinationContainer"/>.</returns>
+    public static async Task CopyBlobsAsync(CloudBlobContainer sourceContainer, CloudBlobContainer destinationContainer, BlobRequestOptions options, CancellationToken cancellationToken) {
+      BlobContinuationToken continuationToken = null;
+
+      do {
+        BlobResultSegment resultSegment = await sourceContainer.ListBlobsSegmentedAsync(null, true, BlobListingDetails.None, MaxNumberOfConcurrentCopyFromBlobOperations, continuationToken, options, null, cancellationToken).ConfigureAwait(false);
+
+        IEnumerable<Task> copyTasks = resultSegment
+            .Results
+            .Cast<CloudBlockBlob>()
+            .Select(
+                sourceBlob => {
+                  CloudBlockBlob destinationBlob = destinationContainer.GetBlockBlobReference(sourceBlob.Name);
+
+                  return CopyBlobAsync(sourceBlob, destinationBlob, options, cancellationToken);
+                });
+
+        await Task.WhenAll(copyTasks).ConfigureAwait(false);
+        cancellationToken.ThrowIfCancellationRequested();
+
+        continuationToken = resultSegment.ContinuationToken;
+      }
+      while (continuationToken != null);
+    }
+
+    /// <summary>
+    /// Returns a <see cref="System.Threading.Tasks.Task"/> instance for the copy blob operation from <paramref name="sourceBlob"/> to <paramref name="destinationBlob"/>.
+    /// </summary>
+    /// <param name="sourceBlob">The <see cref="Microsoft.WindowsAzure.Storage.Blob.CloudBlockBlob"/> instance to be copied to <paramref name="destinationBlob"/>.</param>
+    /// <param name="destinationBlob">The <see cref="Microsoft.WindowsAzure.Storage.Blob.CloudBlockBlob"/> instance where <paramref name="sourceBlob"/> will be copied.</param>
+    /// <param name="options">The <see cref="Microsoft.WindowsAzure.Storage.Blob.BlobRequestOptions"/>.</param>
+    /// <param name="cancellationToken">The <see cref="System.Threading.CancellationToken"/> instance used for cancellation.</param>
+    /// <returns>A <see cref="System.Threading.Tasks.Task"/> instance for the copy blob operation from <paramref name="sourceBlob"/> to <paramref name="destinationBlob"/>.</returns>
+    public static async Task CopyBlobAsync(CloudBlockBlob sourceBlob, CloudBlockBlob destinationBlob, BlobRequestOptions options, CancellationToken cancellationToken) {
+      await destinationBlob.StartCopyAsync(sourceBlob, null, null, options, null, cancellationToken).ConfigureAwait(false);
+
+      CopyState copyState = destinationBlob.CopyState;
+      while (copyState == null || copyState.Status == CopyStatus.Pending) {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        await destinationBlob.FetchAttributesAsync(null, options, null, cancellationToken).ConfigureAwait(false);
+
+        copyState = destinationBlob.CopyState;
+        if (copyState != null && copyState.Status != CopyStatus.Pending && copyState.Status != CopyStatus.Success) {
+          throw new StorageException(copyState.StatusDescription);
+        }
+      }
+    }
+  }
+}
