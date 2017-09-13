393aad5572ad8fd5def97c61d6366ec1dac7cca5
Shawn Mclean <shawnrmclean@gmail.com>
2017-09-12 19:17:08 -0500

    Extracted interface.

--- src/Azure.MediaServices.Core/AzureMediaServiceClient.cs
+++ src/Azure.MediaServices.Core/AzureMediaServiceClient.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
@@ -17,19 +15,19 @@ using Azure.MediaServices.Core.MediaProcessors;
 using Azure.MediaServices.Core.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Newtonsoft.Json.Serialization;
 
 namespace Azure.MediaServices.Core
 {
   public class AzureMediaServiceClient : IAzureMediaServiceClient
   {
-    private readonly HttpClient _httpClient;
     private readonly string _clientId;
     private readonly string _clientSecret;
-    private readonly string _tenantDomain;
+    private readonly HttpClient _httpClient;
     private readonly JsonSerializerSettings _jsonSerializerSettings;
+    private readonly string _tenantDomain;
 
-    private AzureMediaServiceClient(HttpClient httpClient, string clientId, string clientSecret, string restApiUrl, string tenantDomain) : this(clientId, clientSecret, restApiUrl, tenantDomain)
+    private AzureMediaServiceClient(HttpClient httpClient, string clientId, string clientSecret, string restApiUrl,
+      string tenantDomain) : this(clientId, clientSecret, restApiUrl, tenantDomain)
     {
       _httpClient = httpClient;
     }
@@ -39,7 +37,7 @@ namespace Azure.MediaServices.Core
       _clientId = clientId;
       _clientSecret = clientSecret;
       _tenantDomain = tenantDomain;
-      
+
       _jsonSerializerSettings = new JsonSerializerSettings
       {
         ContractResolver = new PrivateSetterContractResolver()
@@ -55,22 +53,22 @@ namespace Azure.MediaServices.Core
       }
     }
 
-    //TODO:
-    // Create asset from blob
-    // Create a job
-    // Get job result/state
-
-    public static async Task<AzureMediaServiceClient> CreateAsync(HttpClient httpClient, string clientId, string clientSecret, string restApiUrl, string tenantDomain)
+    public static async Task<AzureMediaServiceClient> CreateAsync(HttpClient httpClient, string clientId,
+      string clientSecret, string restApiUrl, string tenantDomain)
     {
       var client = new AzureMediaServiceClient(httpClient, clientId, clientSecret, restApiUrl, tenantDomain);
       await client.Initialize();
       return client;
     }
-    public static async Task<AzureMediaServiceClient> CreateAsync(string clientId, string clientSecret, string restApiUrl, string tenantDomain) {
+
+    public static async Task<AzureMediaServiceClient> CreateAsync(string clientId, string clientSecret,
+      string restApiUrl, string tenantDomain)
+    {
       var client = new AzureMediaServiceClient(clientId, clientSecret, restApiUrl, tenantDomain);
       await client.Initialize();
       return client;
     }
+
     private async Task Initialize()
     {
       await SetupJWT();
@@ -80,16 +78,16 @@ namespace Azure.MediaServices.Core
     {
       var body = new
       {
-        Name = job.Name,
+        job.Name,
         InputMediaAssets = job.InputMediaAssets.Select(m => new
         {
-          __metadata = new { uri = m.Metadata.Uri }
+          __metadata = new {uri = m.Metadata.Uri}
         }),
         Tasks = job.Tasks.Select(t => new
         {
-          Configuration = t.Configuration,
-          MediaProcessorId = t.MediaProcessorId,
-          TaskBody = t.TaskBody
+          t.Configuration,
+          t.MediaProcessorId,
+          t.TaskBody
         })
       };
       _httpClient.DefaultRequestHeaders.Remove("DataServiceVersion");
@@ -105,7 +103,9 @@ namespace Azure.MediaServices.Core
     {
       return GetOne<JobResponse>($"Jobs('{Uri.EscapeDataString(id)}')");
     }
-    public Task<List<Asset>> GetJobOutputAsset(string id) {
+
+    public Task<List<Asset>> GetJobOutputAsset(string id)
+    {
       return Get<Asset>($"Jobs('{Uri.EscapeDataString(id)}')/OutputMediaAssets");
     }
 
@@ -114,9 +114,11 @@ namespace Azure.MediaServices.Core
       return Get<Asset>("Assets");
     }
 
-    public Task<List<AssetFile>> GetAssetFiles(string id) {
+    public Task<List<AssetFile>> GetAssetFiles(string id)
+    {
       return Get<AssetFile>($"Assets('{Uri.EscapeDataString(id)}')/Files");
     }
+
     public Task<Asset> CreateAsset(string name, string storageAccountName)
     {
       var body = new
@@ -140,8 +142,10 @@ namespace Azure.MediaServices.Core
       return Post<AccessPolicy>("AccessPolicies", body);
     }
 
-    public Task<Locator> CreateLocator(string accessPolicyId, string assetId, DateTime startTime, int type) {
-      var body = new {
+    public Task<Locator> CreateLocator(string accessPolicyId, string assetId, DateTime startTime, int type)
+    {
+      var body = new
+      {
         AccessPolicyId = accessPolicyId,
         AssetId = assetId,
         StartTime = startTime,
@@ -149,6 +153,7 @@ namespace Azure.MediaServices.Core
       };
       return Post<Locator>("Locators", body);
     }
+
     public Task<AssetFile> CreateAssetFile(string name, string parentAssetId)
     {
       var body = new
@@ -158,8 +163,11 @@ namespace Azure.MediaServices.Core
       };
       return Post<AssetFile>("Files", body);
     }
-    public async Task<AssetFile> UpdateAssetFile(AssetFile file) {
-      var body = new {
+
+    public async Task<AssetFile> UpdateAssetFile(AssetFile file)
+    {
+      var body = new
+      {
         file.Id,
         file.MimeType,
         file.Name,
@@ -170,48 +178,50 @@ namespace Azure.MediaServices.Core
       return file;
     }
 
-    public Task<List<MediaProcessor>> GetMediaProcessors() {
+    public Task<List<MediaProcessor>> GetMediaProcessors()
+    {
       return Get<MediaProcessor>("MediaProcessors");
     }
 
-    internal async Task<List<TResponse>> Get<TResponse>(string path) {
+    internal async Task<List<TResponse>> Get<TResponse>(string path)
+    {
       var response = await _httpClient.GetAsync(path);
       var stringContent = await response.Content.ReadAsStringAsync();
-      if (!response.IsSuccessStatusCode) {
-        throw new WebException("Failed");
-      }
-      var responseObject = JsonConvert.DeserializeObject<ODataResult<TResponse>>(stringContent, _jsonSerializerSettings);
+      if (!response.IsSuccessStatusCode) throw new WebException("Failed");
+      var responseObject =
+        JsonConvert.DeserializeObject<ODataResult<TResponse>>(stringContent, _jsonSerializerSettings);
       return responseObject.D.Results;
     }
-    internal async Task<TResponse> GetOne<TResponse>(string path) {
+
+    internal async Task<TResponse> GetOne<TResponse>(string path)
+    {
       var response = await _httpClient.GetAsync(path);
       var stringContent = await response.Content.ReadAsStringAsync();
-      if (!response.IsSuccessStatusCode) {
-        throw new WebException("Failed");
-      }
-      var responseObject = JsonConvert.DeserializeObject<ODataSingleResult<TResponse>>(stringContent, _jsonSerializerSettings);
+      if (!response.IsSuccessStatusCode) throw new WebException("Failed");
+      var responseObject =
+        JsonConvert.DeserializeObject<ODataSingleResult<TResponse>>(stringContent, _jsonSerializerSettings);
       return responseObject.D;
     }
 
-    internal async Task<TResponse> Post<TResponse>(string path, object body, HttpMethod method = null) where TResponse : class{
+    internal async Task<TResponse> Post<TResponse>(string path, object body, HttpMethod method = null)
+      where TResponse : class
+    {
       var message = new HttpRequestMessage(HttpMethod.Post, Uri.EscapeDataString(path));
       if (method != null)
-      {
         message.Method = method;
-      }
       var bodyContent = JsonConvert.SerializeObject(body, _jsonSerializerSettings);
       message.Content = new StringContent(bodyContent, Encoding.UTF8, "application/json");
       var response = await _httpClient.SendAsync(message);
       var stringContent = await response.Content.ReadAsStringAsync();
-      if (!response.IsSuccessStatusCode) {
-        throw new WebException("Failed");
-      }
+      if (!response.IsSuccessStatusCode) throw new WebException("Failed");
       if (response.StatusCode == HttpStatusCode.NoContent)
-        return await Task.FromResult((TResponse)null);
+        return await Task.FromResult((TResponse) null);
 
-      var responseObject = JsonConvert.DeserializeObject<ODataResponse<TResponse>>(stringContent, _jsonSerializerSettings);
+      var responseObject =
+        JsonConvert.DeserializeObject<ODataResponse<TResponse>>(stringContent, _jsonSerializerSettings);
       return responseObject.D;
     }
+
     internal async Task<List<TResponse>> Send<TResponse>(string path, HttpMethod method, object body)
     {
       var request = new HttpRequestMessage(method, Uri.EscapeDataString(path));
@@ -221,29 +231,30 @@ namespace Azure.MediaServices.Core
       var response = await _httpClient.SendAsync(request);
       var stringContent = await response.Content.ReadAsStringAsync();
       if (!response.IsSuccessStatusCode)
-      {
         throw new WebException("Failed");
-      }
-      var responseObject = JsonConvert.DeserializeObject<ODataResult<TResponse>>(stringContent, _jsonSerializerSettings);
+      var responseObject =
+        JsonConvert.DeserializeObject<ODataResult<TResponse>>(stringContent, _jsonSerializerSettings);
       return responseObject.D.Results;
     }
+
     private async Task SetupJWT()
     {
-      var body = $"resource={HttpUtility.UrlEncode(Constants.Resource)}&client_id={_clientId}&client_secret={HttpUtility.UrlEncode(_clientSecret)}&grant_type=client_credentials";
+      var body =
+        $"resource={HttpUtility.UrlEncode(Constants.Resource)}&client_id={_clientId}&client_secret={HttpUtility.UrlEncode(_clientSecret)}&grant_type=client_credentials";
 
       var httpContent = new StringContent(body, Encoding.UTF8, "application/x-www-form-urlencoded");
-      var response = await _httpClient.PostAsync($"https://login.microsoftonline.com/{_tenantDomain}/oauth2/token", httpContent);
+      var response = await _httpClient.PostAsync($"https://login.microsoftonline.com/{_tenantDomain}/oauth2/token",
+        httpContent);
 
       if (!response.IsSuccessStatusCode) throw new Exception();
-      
+
       var resultBody = await response.Content.ReadAsStringAsync();
 
       var obj = JObject.Parse(resultBody);
 
       if (obj["access_token"] != null)
-      {
-        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", obj["access_token"].ToString());
-      }
+        _httpClient.DefaultRequestHeaders.Authorization =
+          new AuthenticationHeaderValue("Bearer", obj["access_token"].ToString());
     }
   }
 }
--- src/Azure.MediaServices.Core/IAzureMediaServiceClient.cs
+++ src/Azure.MediaServices.Core/IAzureMediaServiceClient.cs
@@ -1,6 +1,26 @@
-﻿namespace Azure.MediaServices.Core
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Azure.MediaServices.Core.AccessPolicies;
+using Azure.MediaServices.Core.Assets;
+using Azure.MediaServices.Core.Jobs;
+using Azure.MediaServices.Core.Locators;
+using Azure.MediaServices.Core.MediaProcessors;
+
+namespace Azure.MediaServices.Core
 {
   public interface IAzureMediaServiceClient
   {
+    Task<AccessPolicy> CreateAccessPolicy(string name, int duration, int permissions);
+    Task<Asset> CreateAsset(string name, string storageAccountName);
+    Task<AssetFile> CreateAssetFile(string name, string parentAssetId);
+    Task<JobResponse> CreateJob(Job job);
+    Task<Locator> CreateLocator(string accessPolicyId, string assetId, DateTime startTime, int type);
+    Task<List<AssetFile>> GetAssetFiles(string id);
+    Task<List<Asset>> GetAssets();
+    Task<JobResponse> GetJob(string id);
+    Task<List<Asset>> GetJobOutputAsset(string id);
+    Task<List<MediaProcessor>> GetMediaProcessors();
+    Task<AssetFile> UpdateAssetFile(AssetFile file);
   }
 }
