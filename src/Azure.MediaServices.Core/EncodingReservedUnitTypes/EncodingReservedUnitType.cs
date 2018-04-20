eed42bc757090a17eb6155697a39ca7dc7fe0ae6
Niklas Arbin <niklas.arbin@bookbeat.com>
2018-04-20 14:51:26 +0200

    Move expanded code from my own private solution into the main project. Includes some new types and lookups and also uses the AdalTokenClient for authentication.

---
+++ src/Azure.MediaServices.Core/EncodingReservedUnitTypes/EncodingReservedUnitType.cs
@@ -0,0 +1,10 @@
+﻿namespace Azure.MediaServices.Core.EncodingReservedUnitTypes
+{
+  public class EncodingReservedUnitType
+  {
+    public string AccountId { get; set; }
+    public int ReservedUnitType { get; set; }
+    public int MaxReservableUnits { get; set; }
+    public int CurrentReservedUnits { get; set; }
+  }
+}
